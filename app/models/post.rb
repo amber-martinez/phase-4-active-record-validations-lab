58352b10900205a65c44f20590d3af5317be915d
Amber <martinez.amber1028@gmail.com>
2022-11-15 17:05:44 -0800

    commit tree 05a40f1847eccd6e57ba08c22bce85bc5fbf4430

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :must_include_in_title
+
+    def must_include_in_title
+
+        allowed_titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+        if allowed_titles.none? { |title_string| title_string.match?(title) }
+            errors.add(:title, "Your title must include 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'. 💩" )
+        end
+
+        # allowed_titles.each do |title_string|
+        #     if title_string.match?(title)
+        #     else
+        #         errors.add(:title, "Your title must include 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'. 💩" )
+        #     end
+        # end
+
+        # if title.match?(/Won't Believe/)
+        # else
+        #     errors.add(:title, "Your title must include 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'. 💩" )
+        # end
+    end
+
 end
